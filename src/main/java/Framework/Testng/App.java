e932a75ca999aa0a9d7257b9fc62938625417bdf
heenaF199 <hinahumayun23@gmail.com>
2024-09-24 16:29:16 +0500

    initial code

---
+++ src/main/java/Framework/Testng/App.java
@@ -0,0 +1,12 @@
+package Framework.Testng;
+
+/**
+ * Hello world!
+ */
+
+public class App {
+   @Test
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/Framework/Testng/class01.java
@@ -0,0 +1,11 @@
+package Framework.Testng;
+
+public class class01 {
+	
+	@Test
+	public void test01() {
+		
+		System.out.println("done");
+	}
+
+}
